b247a85e54ad6a82152be4291ecf15e5e3a7fee8
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-12-19 15:04:07 -0300

    cors fix

---
+++ src/main/java/com/doctortime/doctortime/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.doctortime.doctortime.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://doctortime-api.onrender.com") // Adicione o endereço permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Cabeçalhos permitidos
+                .allowCredentials(true); // Permitir cookies e autenticação
+    }
+}
